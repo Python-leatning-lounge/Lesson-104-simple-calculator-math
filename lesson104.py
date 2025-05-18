32b9e42653b2a7f758e8b17780d5333461ab8c07
César Gabriel Morín Corona <121568700+Cesar-Gabriel@users.noreply.github.com>
2025-05-17 18:04:41 -0600

    Create lesson104.py

---
+++ lesson104.py
@@ -0,0 +1,10 @@
+# remember that # makes comments so depending if you are using and IDE or KATE or VSCODE lines with # are gonna be ignored
+# on this lesson we are going to learn how to do math
+# we are going to do this with variables like a
+a = 158
+b = 200
+# Now we got the variables ready, we got two options
+print(a + b)
+# Or use another variable
+c = a + b
+print(c)
