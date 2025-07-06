8d31db4573de4f8af48c9c13d441a87f5ea6544e
César Gabriel Morín Corona <121568700+Cesar-Gabriel@users.noreply.github.com>
2025-07-06 11:04:35 -0600

    Update lesson104.py

--- lesson104.py
+++ lesson104.py
@@ -6,3 +6,96 @@ b = 200
 # Now we got the variables ready
 print(a + b)
 # Reminder that putting a quote just puts it as text
+
+# Now we have the othe option, which is using ANOTHER variable
+# I will use c as a example of this lesson
+# With different numbers
+a = 293
+b = 395
+c = a + b
+# Now we can print c
+print(c)
+# Both give the same result from the previous print if we used the same numbers
+
+# Now, let's do divisions
+# I will use the same variables but with different numbers
+a = 1000
+b = 500
+c = a / b
+# Now we can print c
+print(c)
+# This will give us 2.0 because it is a float division
+# If we want to do a integer division we can use //
+c = a // b
+# Now we can print c
+print(c)
+# This will give us 2 because it is a integer division
+# Now let's do a multiplication
+a = 10
+b = 5
+c = a * b
+# Now we can print c
+print(c)
+# This will give us 50 because it is a multiplication
+
+# And for minus is the same as the first one but with a minus instead of a plus
+a = 103
+b = 506
+c = a - b
+# Now we can print c
+print(c)
+# This will give us -403 because it is a subtraction
+# And that would be all for this lesson
+# Remember that you can use any variable name you want, but it is recommended to use a meaningful name
+# Also remember that you can use any number you want, but it is recommended to use numbers that are easy to remember
+# And that is all for this lesson, I hope you enjoyed it and learned something new
+# If you have any questions, feel free to ask me
+# See you in the next lesson!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# No copilot for VSCODE, i don't want to make a modulo, let's keep it simple
+# do you understand, copilot?
+# I hope you do, because this is a simple lesson not math 55 Hardvard
